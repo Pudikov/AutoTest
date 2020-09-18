0497bf6612a6780ec69f4d3d75396bbbcc758d92
pudikov <d.pudikov@mail.ru>
2020-09-18 23:36:43 +0300

    3 задание

---
+++ src/main/java/lesson1/task3/Logic.java
@@ -0,0 +1,19 @@
+package lesson1.task3;
+
+public class Logic {
+    boolean x;
+    public static void main(String[] args) {
+    Logic inst = new Logic();
+    System.out.println(inst.logic(14));
+    System.out.println(inst.logic(19));
+
+    }
+    boolean logic (int n) {
+        int m = 0;
+        if (m == n % 2)
+            x = false;
+        else
+            x = true;
+        return x;
+    }
+}
