6c0b957811906a7f654bcf2e910cffb8a48aba14
pudikov <d.pudikov@mail.ru>
2020-09-21 21:14:06 +0300

    1 задание

--- src/main/java/lesson2/task1/Friday.java
+++ src/main/java/lesson2/task1/Friday.java
@@ -6,9 +6,10 @@ public class Friday {
     public static void main(String[] args) {
         Friday inst = new Friday();
         inst.printFor();
-        inst.printForEech();
+        inst.printForEach();
         inst.printWh();
     }
+    // Вывод на экран FOR
     char printFor () {
         for (int i = 0; i < week.length; i++) {
             System.out.print(week [i]);
@@ -16,15 +17,16 @@ public class Friday {
         System.out.println();
         return 0;
     }
-    char printForEech () {
-        int i =0;
-        for (String string: week) {
-            System.out.print(week[i]);
-            i++;
+    // Вывод на экран FOR Each
+    char printForEach () {
+        int i=0;
+        for (String s:week) {System.out.print(week[i]);
+        i++;
         }
         System.out.println();
         return 0;
     }
+    // Вывод на экран While
     char printWh (){
         int i = 0;
         while (i<week.length){
