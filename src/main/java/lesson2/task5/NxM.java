2ab8982ddb13b1aa1f9197f24b039b8097302012
pudikov <d.pudikov@mail.ru>
2020-09-23 16:28:55 +0300

    6 заданий,3 задача не полностью

--- src/main/java/lesson2/task5/NxM.java
+++ src/main/java/lesson2/task5/NxM.java
@@ -4,30 +4,32 @@ import java.util.Random;
 
 public class NxM {
     public static void main(String[] args) {
-        int [][] nm = new int[3][5];
-        for (int i = 0; i < nm.length; i++){
+        int[][] nm = new int[3][5];
+        for (int i = 0; i < nm.length; i++) {
             for (int j = 0; j < nm.length; j++)
-                nm[i][j] = (int) (Math.random()*10);
+                nm[i][j] = (int) (Math.random() * 10);
         }
-        for (int i = 0; i < nm.length; i++){
-            for (int j = 0; j < nm.length; j++){
-            System.out.print(nm[i][j]);}
+        for (int i = 0; i < nm.length; i++) {
+            for (int j = 0; j < nm.length; j++) {
+                System.out.print(nm[i][j]);
+            }
             System.out.println();
         }
         for (int i = 0; i < nm.length; i++) {
-            for (int j = i+1; j < nm.length; j++) {
+            for (int j = i + 1; j < nm.length; j++) {
                 int temp = nm[i][j];
                 nm[i][j] = nm[j][i];
                 nm[j][i] = temp;
             }
         }
         System.out.println();
-        for (int i = 0; i < nm.length; i++){
-            for (int j = 0; j < nm.length; j++){
-                System.out.print(nm[i][j]);}
+        for (int i = 0; i < nm.length; i++) {
+            for (int j = 0; j < nm.length; j++) {
+                System.out.print(nm[i][j]);
+            }
             System.out.println();
         }
-        }
-
     }
+
+}
 
