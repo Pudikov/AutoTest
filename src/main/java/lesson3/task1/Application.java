6782b6b354afca9cd8be85c3e92315b69e6c2e6d
pudikov <d.pudikov@mail.ru>
2020-09-26 22:14:37 +0300

    1 задание

---
+++ src/main/java/lesson3/task1/Application.java
@@ -0,0 +1,11 @@
+package lesson3.task1;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.summ(3, 5);
+        calculator.substract(3, 6);
+        calculator.multiply(6,8);
+        calculator.divide(4,2);
+    }
+}
