ab05d6685ac19ea7beaccad65d00ddeb4502bbb1
pudikov <d.pudikov@mail.ru>
2020-09-30 16:41:38 +0300

    4 задания

---
+++ src/main/java/lesson3/task4/Application.java
@@ -0,0 +1,42 @@
+package lesson3.task4;
+
+public class Application {
+
+
+    public static void main(String[] args) {
+        Human sergey = new Human("Сергей");
+        Human michail = new Human("Михаил");
+        Human egor = new Human("Егор");
+        Human aleksandr = new Human("Александр");
+        Human valeriy = new Human("Валерий");
+        Human stanislav = new Human("Станислав");
+        Human grigoriy = new Human("Григорий");
+// создание связей
+        sergey.addFriends(valeriy);
+        sergey.addFriends(grigoriy);
+        stanislav.addFriends(valeriy);
+        stanislav.addFriends(michail);
+        egor.addFriends(aleksandr);
+        egor.addFriends(grigoriy);
+        aleksandr.addFriends(grigoriy);
+        aleksandr.addFriends(michail);
+        michail.addFriends(stanislav);
+        michail.addFriends(aleksandr);
+
+        System.out.println(isAlFr(sergey, grigoriy));
+        System.out.println(isAlFr(sergey, aleksandr));
+        System.out.println(isAlFr(stanislav, michail));
+        System.out.println(isAlFr(michail, valeriy));
+
+    }
+// метод для проверки связей
+    // static boolean isAlreadyFriend = false;
+
+    static boolean isAlFr(Human name1, Human name2) {
+        for (Human human : name1.friends)
+            if (human == name2) return true;
+return false;
+
+
+    }
+}
---
+++ src/main/java/lesson3/task4/Human.java
@@ -0,0 +1,23 @@
+package lesson3.task4;
+
+import java.util.Arrays;
+
+public class Human {
+    String name;
+    Human[] friends;
+
+    Human(String name) {
+        this.name = name;
+    }
+//  Добовления друга
+    void addFriends(Human addHumen) {
+        if (friends == null) // проверка что  пустой
+            friends = new Human[1]; // инициализация массива
+        else {
+            friends = Arrays.copyOf(friends, friends.length + 1); // копирование массива
+        }
+        friends[friends.length - 1] = addHumen; // добовление элемента
+
+
+    }
+}
